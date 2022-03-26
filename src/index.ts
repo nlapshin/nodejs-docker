7d17cc4a515bf237ab3485a25d9c55164b9ccc7a
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-03-26 20:05:24 +0300

    Simple project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import config from './config'
+import router from './routes'
+
+const app = express();
+app.use(router);
+
+app.listen(config.port, () => {
+  console.log(`server running on port 4000`);
+});
